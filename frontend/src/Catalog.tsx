72ea3fd480fb7104c31ba5f284bd56e8a22a4309
Richard Yu <richardyu@berkeley.edu>
2024-06-02 14:16:33 -0700

    Implement room listing selection

--- frontend/src/Catalog.tsx
+++ frontend/src/Catalog.tsx
@@ -1,8 +1,37 @@
+import { useEffect, useState } from "react";
 import Filters from "./components/Filters";
 import Room from "./components/Room";
 import RoomListing from "./components/RoomListing";
 
 export default function Catalog() {
+    const [roomSearchData, setRoomSearchData] = useState(null);
+    const [catalogLoading, setCatalogLoading] = useState(true);
+    const [selectedRoom, setSelectedRoom] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("http://127.0.0.1:8000/rooms");
+                if (!response.ok) {
+                    throw new Error(`HTTP error ${response.status}`);
+                }
+                const result = await response.json();
+                setRoomSearchData(result);
+                console.log(result);
+            } catch (error) {
+                console.log(error);
+            } finally {
+                setCatalogLoading(false);
+            }
+        };
+    
+        fetchData();
+    }, []);
+
+    const handleRoomSelection = (roomID) => {
+        setSelectedRoom(roomID);
+    }
+
     return (
         <>
             <div className="flex h-screen">
@@ -10,12 +39,22 @@ export default function Catalog() {
                     <Filters />
                 </div>
                 <div className="w-1/4 p-4 overflow-y-auto border-r">
-                    <Room roomID={-1} displayName="Room" status="Closed" />
-                    <Room roomID={-1} displayName="Room" status="Unreserved" />
-                    <Room roomID={-1} displayName="Room" status="Reserved" />
+                    {
+                        !catalogLoading ?
+                            roomSearchData.map((item, index) => (
+                                <div onClick={() => handleRoomSelection(item.room_id)}>
+                                    <Room roomID={item.room_id} displayName={item.room_name_display} status={item.reservation_status} />
+                                </div>
+                            ))
+                        : <></>
+                    }
                 </div>
-                <div className="w-1/2 p-4">
-                    <RoomListing roomID={-1} />
+                <div className="w-1/2 p-4 overflow-y-auto">
+                    {
+                        selectedRoom ?
+                            <RoomListing roomID={selectedRoom} />
+                        : <h3 className="text-xl text-center text-gray-400">Room not selected</h3>
+                    }
                 </div>
             </div>
         </>
--- frontend/src/components/RoomListing.tsx
+++ frontend/src/components/RoomListing.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 interface RoomListingProps {
     roomID: number;
@@ -6,13 +6,49 @@ interface RoomListingProps {
 
 export default function RoomListing({ roomID }: RoomListingProps) {
     const [roomData, setRoomData] = useState(null);
+    const [roomListingLoading, setRoomListingLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const roomResponse = await fetch(`http://127.0.0.1:8000/rooms/${roomID}`);
+                if (!roomResponse.ok) {
+                    throw new Error(`HTTP error ${roomResponse.status}`);
+                }
+                const roomData = await roomResponse.json();
+                setRoomData(roomData);
+                console.log(roomData);
+            } catch (error) {
+                console.log(error);
+            } finally {
+                setRoomListingLoading(false);
+            }
+        };
+    
+        fetchData();
+    }, [roomID]);
 
     return (
         <div className="p-4">
-            <h1 className="text-3xl font-bold">Room</h1>
-            <h3 className="text-lg">Room Number</h3>
-            <h2 className="text-2xl font-bold mt-4">Features</h2>
-            <h2 className="text-2xl font-bold mt-4">Schedule</h2>
+            { !roomListingLoading ?
+                <>
+                    <h1 className="text-3xl font-bold">{roomData.room_name_display.split(",")[0]}</h1>
+                    <h3 className="text-lg">Room {roomData.room_number}</h3>
+                    <div>
+                        <h2 className="text-2xl font-bold mt-4">Features</h2>
+                        <p className="text-lg">Capacity {roomData.room_capacity}</p>
+                        <br />
+                        {
+                            roomData.room_features.map((item, index) => (
+                                <p className="text-lg">{item}</p>
+                            ))
+                        }
+                    </div>
+                    <h2 className="text-2xl font-bold mt-4">Schedule</h2>
+                    
+                </>
+                : <></>
+            }
         </div>
     )
 }
