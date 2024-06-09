a54183e160a13c3699a04232a0c14d938a172653
Richard Yu <richardyu@berkeley.edu>
2024-06-08 20:25:32 -0700

    Fetch filters from api

--- frontend/src/components/Filters.tsx
+++ frontend/src/components/Filters.tsx
@@ -1,29 +1,85 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import Calendar from "./Calendar";
 import Dropdown from "./Dropdown";
 
-export default function Filters() {
+export default function Filters({ data, filterUpdate }) {
     const [startTime, setStartTime] = useState("")
     const [endTime, setEndTime] = useState("")
     const [date, setDate] = useState(new Date())
     const [buildings, setBuildings] = useState("")
     const [features, setFeatures] = useState("")
-    const [type, setType] = useState("")
+    const [categories, setCategories] = useState("")
     const [status, setStatus] = useState("")
+
+    const [filters, setFilters] = useState(null);
+    const [filtersLoaded, setFiltersLoaded] = useState(false);
+
+    // const uniqueBuildings = [...new Set(data.map(obj => obj.room_name_display.split(",")[0]))].sort();
+    const uniqueStatuses = [...new Set(data.map(obj => obj.reservation_status))];
+
+    const prevFilterState = useRef({
+        startTime: "",
+        endTime: "",
+        date: new Date(),
+        buildings: "",
+        features: "",
+        type: "",
+        status: ""
+    });
+
+    useEffect(() => {
+        if (
+            prevFilterState.current.startTime !== startTime ||
+            prevFilterState.current.endTime !== endTime ||
+            prevFilterState.current.date !== date ||
+            prevFilterState.current.buildings !== buildings ||
+            prevFilterState.current.features !== features ||
+            prevFilterState.current.status !== status
+        ) {
+            filterUpdate({
+                startTime,
+                endTime,
+                date,
+                buildings,
+                features,
+                status
+            });
+
+            prevFilterState.current = {
+                startTime,
+                endTime,
+                date,
+                buildings,
+                features,
+                status
+            };
+        }
+    }, [startTime, endTime, date, buildings, features, status, filterUpdate]);
+    
+    useEffect(() => {
+        fetch("http://localhost:8000/filters")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setFilters(data);
+                setFiltersLoaded(true);
+            })
+            .catch(err => console.error(err));
+    }, [])
 
     return (
         <div className="w-full">
             <h2 className="text-3xl font-bold">Filters</h2>
             <div className="flex">
-                <Dropdown dropdownText="Start" options={["Test"]} selectDropdownItem={setStartTime} />
+                <Dropdown dropdownText="Start" options={[]} selectDropdownItem={setStartTime} />
                 <div className="w-4"></div>
-                <Dropdown dropdownText="End" options={["Test"]} selectDropdownItem={setEndTime} />
+                <Dropdown dropdownText="End" options={[]} selectDropdownItem={setEndTime} />
             </div>
             <Calendar onDateClick={setDate} />
-            <Dropdown dropdownText="Buildings" options={[]} selectDropdownItem={setBuildings} />
-            <Dropdown dropdownText="Features" options={[]} selectDropdownItem={setFeatures} />
-            <Dropdown dropdownText="Type" options={[]} selectDropdownItem={setType} />
-            <Dropdown dropdownText="Status" options={[]} selectDropdownItem={setStatus}  />
+            <Dropdown dropdownText="Buildings" options={filtersLoaded ? filters.buildings.map(building => building.display_name) : []} selectDropdownItem={setBuildings} />
+            <Dropdown dropdownText="Features" options={filtersLoaded ? filters.features : []} selectDropdownItem={setFeatures} />
+            <Dropdown dropdownText="Categories" options={filtersLoaded ? filters.categories : []} selectDropdownItem={setCategories} />
+            <Dropdown dropdownText="Status" options={["Unreserved", "Reserved", "Closed"]} selectDropdownItem={setStatus} />
         </div>
     );
 }
