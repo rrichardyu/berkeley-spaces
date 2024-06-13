5dd750417fc2a7c543044b9eae6eba59ee44bb9f
Richard Yu <richardyu@berkeley.edu>
2024-06-12 19:53:38 -0700

    Setup environment variables

--- api/database.py
+++ api/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql://postgres:cal@db:5432/postgres"
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(DATABASE_URL, echo=True)
 
